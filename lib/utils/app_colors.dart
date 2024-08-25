a059f1d397682bce54132915fa844e036b081143
ZiadElgaraihe <ziadelgaraihe301@gmail.com>
2024-08-25 19:48:53 +0300

    create app colors class

---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  ///#EDF2F7 (box)
+  static const Color antiFlashWhite = Color(0xFFEDF2F7);
+
+  ///#1E1E1E (text color)
+  static const Color black = Color(0xFF1E1E1E);
+
+  ///#2F80EC (link)
+  static const Color blue = Color(0xFF2F80EC);
+
+  ///#27AE61 (success)
+  static const Color green = Color(0xFF27AE61);
+
+  ///#E1E8FF (second btn)
+  static const Color lavender = Color(0xFFE1E8FF);
+
+  ///#F39A4A (orange text)
+  static const Color orange = Color(0xFFF39A4A);
+
+  ///#092C4C (main color)
+  static const Color prussianBlue = Color(0xFF092C4C);
+
+  ///#EB5757 (wrong)
+  static const Color red = Color(0xFFEB5757);
+
+  ///#58B47F
+  static const Color secondGreen = Color(0xFF58B47F);
+
+  ///#FFFFFF
+  static const Color white = Color(0xFFFFFFFF);
+
+  ///#E2B93B (warning)
+  static const Color yellow = Color(0xFFE2B93B);
+}
