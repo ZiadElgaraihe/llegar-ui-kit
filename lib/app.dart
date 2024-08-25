e7398769ae44ef566a93bcb7a1c94d4104366402
ZiadElgaraihe <ziadelgaraihe301@gmail.com>
2024-08-25 22:20:02 +0300

    use light theme and dark theme in material app

--- lib/app.dart
+++ lib/app.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:llegar/utils/app_themes.dart';
 
 class Llegar extends StatelessWidget {
   const Llegar({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp();
+    return MaterialApp(
+      themeMode: ThemeMode.light,
+      theme: AppThemes.lightTheme,
+      darkTheme: AppThemes.darkTheme,
+    );
   }
 }
