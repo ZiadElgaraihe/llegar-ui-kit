ba2f560c3f952f74565db7a1e3eeb1a314654e46
ZiadElgaraihe <ziadelgaraihe301@gmail.com>
2024-08-27 19:31:30 +0300

    add some images & icons

--- lib/utils/app_icons.dart
+++ lib/utils/app_icons.dart
@@ -1,2 +1,8 @@
-//This class contains all app icons like (arrows, search, notifications, ...)
-abstract class AppIcons {}
+///This class contains all app icons like (arrows, search, notifications, ...)
+abstract class AppIcons {
+  ///assets/icons/arrow_left.svg
+  static const String arrowLeft = 'assets/icons/arrow_left.svg';
+
+  ///assets/icons/arrow_right.svg
+  static const String arrowRight = 'assets/icons/arrow_right.svg';
+}
--- lib/utils/app_images.dart
+++ lib/utils/app_images.dart
@@ -1,2 +1,14 @@
-//This class contains all app images
-abstract class AppImages {}
+///This class contains all app images
+abstract class AppImages {
+  ///assets/images/make_deal.svg
+  static const String makeDeal = 'assets/images/make_deal.svg';
+
+  ///assets/images/secure_app.svg
+  static const String secureApp = 'assets/images/secure_app.svg';
+
+  ///assets/images/two_factor_auth.svg
+  static const String twoFactorAuth = 'assets/images/two_factor_auth.svg';
+
+  ///assets/images/welcome_to_llegar.svg
+  static const String welcomeToLlegar = 'assets/images/welcome_to_llegar.svg';
+}
