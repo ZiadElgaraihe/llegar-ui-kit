1a41a74431ea376d13c194e1087892a7dd6e3678
ZiadElgaraihe <ziadelgaraihe301@gmail.com>
2024-08-27 18:49:04 +0300

    add font families names to app strings

--- lib/utils/app_strings.dart
+++ lib/utils/app_strings.dart
@@ -2,4 +2,6 @@
 ///like app name, keys, fonts and more
 abstract class AppStrings {
   static const String appName = 'Llegar';
+  static const String urbanistFontFamily = 'Urbanist';
+  static const String interFontFamily = 'Inter';
 }
