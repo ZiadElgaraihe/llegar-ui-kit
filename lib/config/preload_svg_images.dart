fd721a371944a77feb81f9eb6916f95082ce2933
ZiadElgaraihe <ziadelgaraihe301@gmail.com>
2024-08-28 23:25:05 +0300

    add preload svg images

---
+++ lib/config/preload_svg_images.dart
@@ -0,0 +1,19 @@
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:llegar/utils/app_images.dart';
+
+///This method load svg images and cache them
+///So this cache helps to avoid reloading the SVG from the asset bundle 
+///each time it's needed, which improves performance
+Future<void> preloadSvgImages() async {
+  const List<String> svgImages = [
+    AppImages.welcomeToLlegar,
+    AppImages.secureApp,
+    AppImages.makeDeal,
+    AppImages.twoFactorAuth,
+  ];
+
+  await Future.wait(svgImages.map((svgPath) {
+    final loader = SvgAssetLoader(svgPath);
+    return svg.cache.putIfAbsent(loader.cacheKey(null), () => loader.loadBytes(null));
+  }));
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:llegar/app.dart';
+import 'package:llegar/config/preload_svg_images.dart';
 
-void main() {
+void main() async {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
+  await preloadSvgImages();
+
+  FlutterNativeSplash.remove();
+
   runApp(const Llegar());
 }
