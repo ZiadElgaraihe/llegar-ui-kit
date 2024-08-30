2b630603914a8d3150a15306769b8b4b71a386e3
ZiadElgaraihe <ziadelgaraihe301@gmail.com>
2024-08-30 15:16:49 +0300

    add bloc provider on material app

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:llegar/core/presentation/manager/cubits/theme_cubit/theme_cubit.dart';
 import 'package:llegar/localization/generated/app_localizations.dart';
 import 'package:llegar/modules/auth/presentation/views/welcome_view.dart';
 import 'package:llegar/utils/app_strings.dart';
@@ -9,16 +11,27 @@ class Llegar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: AppStrings.appName,
-      debugShowCheckedModeBanner: false,
-      themeMode: AppThemes.themeMode,
-      theme: AppThemes.lightTheme(context),
-      darkTheme: AppThemes.darkTheme(context),
-      locale: const Locale('en'),
-      localizationsDelegates: AppLocalizations.localizationsDelegates,
-      supportedLocales: AppLocalizations.supportedLocales,
-      home: const WelcomeView(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<ThemeCubit>(
+          create: (context) => ThemeCubit()..setUpThemeMode(),
+        ),
+      ],
+      child: Builder(
+        builder: (context) {
+          return MaterialApp(
+            title: AppStrings.appName,
+            debugShowCheckedModeBanner: false,
+            themeMode: BlocProvider.of<ThemeCubit>(context).themeMode,
+            theme: AppThemes.lightTheme(context),
+            darkTheme: AppThemes.darkTheme(context),
+            locale: const Locale('en'),
+            localizationsDelegates: AppLocalizations.localizationsDelegates,
+            supportedLocales: AppLocalizations.supportedLocales,
+            home: const WelcomeView(),
+          );
+        }
+      ),
     );
   }
 }
