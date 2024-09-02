33495151eb31e45e93e570c1b9c3562573828058
ZiadElgaraihe <ziadelgaraihe301@gmail.com>
2024-09-02 22:57:31 +0300

    preload splash view image before native splash is removed

--- lib/app.dart
+++ lib/app.dart
@@ -1,43 +1,58 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:llegar/config/preload_svg_images.dart';
 import 'package:llegar/core/presentation/manager/cubits/locale_cubit/locale_cubit.dart';
 import 'package:llegar/core/presentation/manager/cubits/theme_cubit/theme_cubit.dart';
 import 'package:llegar/localization/generated/app_localizations.dart';
 import 'package:llegar/modules/splash/presentation/views/splash_view.dart';
+import 'package:llegar/utils/app_images.dart';
 import 'package:llegar/utils/app_routes.dart';
 import 'package:llegar/utils/app_strings.dart';
 import 'package:llegar/utils/app_themes.dart';
+import 'package:llegar/utils/functions/value_based_on_theme.dart';
 
-class Llegar extends StatelessWidget {
+class Llegar extends StatefulWidget {
   const Llegar({super.key});
 
   @override
+  State<Llegar> createState() => _LlegarState();
+}
+
+class _LlegarState extends State<Llegar> {
+  @override
+  void initState() {
+    super.initState();
+    preloadSvgImages(
+      svgImages: valueBasedOnTheme<List<String>>(
+        context,
+        light: [
+          AppImages.splashLogo,
+        ],
+        dark: [
+          AppImages.splashLogoDark,
+        ],
+      )!,
+    ).then(
+      (value) {
+        FlutterNativeSplash.remove();
+      },
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider<ThemeCubit>(
-          create: (context) => ThemeCubit()..setUpThemeMode(),
-        ),
-        BlocProvider<LocaleCubit>(
-          create: (context) => LocaleCubit()..setUpLocale(),
-        ),
-      ],
-      child: Builder(
-        builder: (context) {
-          return MaterialApp(
-            title: AppStrings.appName,
-            debugShowCheckedModeBanner: false,
-            themeMode: BlocProvider.of<ThemeCubit>(context).themeMode,
-            theme: AppThemes.lightTheme(context),
-            darkTheme: AppThemes.darkTheme(context),
-            locale: BlocProvider.of<LocaleCubit>(context).locale,
-            localizationsDelegates: AppLocalizations.localizationsDelegates,
-            supportedLocales: AppLocalizations.supportedLocales,
-            onGenerateRoute: AppRoutes.generate,
-            home: const SplashView(),
-          );
-        },
-      ),
+    return MaterialApp(
+      title: AppStrings.appName,
+      debugShowCheckedModeBanner: false,
+      themeMode: BlocProvider.of<ThemeCubit>(context).themeMode,
+      theme: AppThemes.lightTheme(context),
+      darkTheme: AppThemes.darkTheme(context),
+      locale: BlocProvider.of<LocaleCubit>(context).locale,
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
+      onGenerateRoute: AppRoutes.generate,
+      home: const SplashView(),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:llegar/app.dart';
+import 'package:llegar/core/presentation/manager/cubits/locale_cubit/locale_cubit.dart';
+import 'package:llegar/core/presentation/manager/cubits/theme_cubit/theme_cubit.dart';
 
 void main() {
-  runApp(const Llegar());
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
+  runApp(
+    MultiBlocProvider(
+      providers: [
+        BlocProvider<ThemeCubit>(
+          create: (context) => ThemeCubit()..setUpThemeMode(),
+        ),
+        BlocProvider<LocaleCubit>(
+          create: (context) => LocaleCubit()..setUpLocale(),
+        ),
+      ],
+      child: const Llegar(),
+    ),
+  );
 }
