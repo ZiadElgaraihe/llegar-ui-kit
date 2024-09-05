a156a7cc5ccb365a55d2f7775b8f8cdad3276756
ZiadElgaraihe <ziadelgaraihe301@gmail.com>
2024-09-05 18:44:49 +0300

    create otp view

--- lib/modules/auth/presentation/widgets/forget_password_view_body.dart
+++ lib/modules/auth/presentation/widgets/forget_password_view_body.dart
@@ -4,6 +4,7 @@ import 'package:llegar/core/presentation/widgets/custom_elevated_button.dart';
 import 'package:llegar/core/presentation/widgets/custom_text_form_field.dart';
 import 'package:llegar/utils/app_colors.dart';
 import 'package:llegar/utils/app_images.dart';
+import 'package:llegar/utils/app_routes.dart';
 import 'package:llegar/utils/app_sizes.dart';
 import 'package:llegar/utils/app_text_styles.dart';
 import 'package:llegar/utils/functions/translate.dart';
@@ -50,7 +51,16 @@ class ForgetPasswordViewBody extends StatelessWidget {
             child: Column(
               children: [
                 const Expanded(child: AppSizes.height24),
-                CustomElevatedButton(title: translate(context).continueText),
+                CustomElevatedButton(
+                  title: translate(context).continueText,
+                  onFuturePressed: () async {
+                    //remove this & add your logic
+                    await Future.delayed(const Duration(seconds: 2));
+
+                    if (!context.mounted) return;
+                    Navigator.pushNamed(context, AppRoutes.otpView);
+                  },
+                ),
                 AppSizes.height24,
               ],
             ),
