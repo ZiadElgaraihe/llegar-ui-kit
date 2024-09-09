fbc8fff6b286147ac16941fce74534e1aebba1b1
ZiadElgaraihe <ziadelgaraihe301@gmail.com>
2024-09-09 21:50:41 +0300

    create custom animated dropdown

---
+++ lib/core/presentation/widgets/custom_animated_dropdown.dart
@@ -0,0 +1,79 @@
+import 'package:animated_custom_dropdown/custom_dropdown.dart';
+import 'package:flutter/material.dart';
+import 'package:llegar/utils/app_colors.dart';
+import 'package:llegar/utils/app_strings.dart';
+import 'package:llegar/utils/app_text_styles.dart';
+import 'package:llegar/utils/functions/value_based_on_theme.dart';
+
+class CustomAnimatedDropdown<ItemsType> extends StatelessWidget {
+  const CustomAnimatedDropdown({
+    super.key,
+    required this.hintText,
+    required this.items,
+    this.onChanged,
+    this.visibility,
+  });
+
+  final String hintText;
+  final List<ItemsType> items;
+  final void Function(ItemsType? newValue)? onChanged;
+  final void Function(bool isListOpened)? visibility;
+
+  @override
+  Widget build(BuildContext context) {
+    return Theme(
+      data: ThemeData(
+        inputDecorationTheme: const InputDecorationTheme(filled: false),
+      ),
+      child: CustomDropdown<ItemsType>(
+        items: items,
+        hintText: hintText,
+        onChanged: onChanged,
+        closedHeaderPadding: const EdgeInsets.symmetric(
+          horizontal: 12,
+          vertical: 8,
+        ),
+        expandedHeaderPadding: const EdgeInsets.symmetric(
+          horizontal: 12,
+          vertical: 8,
+        ),
+        visibility: visibility,
+        decoration: CustomDropdownDecoration(
+          closedFillColor: AppColors.white,
+          hintStyle: AppTextStyles.medium14(context).copyWith(
+            fontFamily: AppStrings.interFontFamily,
+            color: AppColors.grey,
+          ),
+          headerStyle: AppTextStyles.medium14(context).copyWith(
+            fontFamily: AppStrings.interFontFamily,
+            color: valueBasedOnTheme<Color>(
+              context,
+              light: AppColors.prussianBlue,
+              dark: AppColors.orange,
+            ),
+          ),
+          listItemStyle: AppTextStyles.medium14(context).copyWith(
+            fontFamily: AppStrings.interFontFamily,
+            color: valueBasedOnTheme<Color>(
+              context,
+              light: AppColors.prussianBlue,
+              dark: AppColors.orange,
+            ),
+          ),
+          closedSuffixIcon: const Icon(
+            Icons.arrow_drop_down_rounded,
+            color: AppColors.grey,
+          ),
+          expandedSuffixIcon: const Icon(
+            Icons.arrow_drop_down_rounded,
+            color: AppColors.grey,
+          ),
+          closedBorderRadius: BorderRadius.circular(8),
+          expandedBorderRadius: BorderRadius.circular(8),
+          closedBorder: Border.all(color: AppColors.grey),
+          expandedBorder: Border.all(color: AppColors.yellow),
+        ),
+      ),
+    );
+  }
+}
