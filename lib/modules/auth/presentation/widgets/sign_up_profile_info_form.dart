9207368d6f10fecdd27203505f7a8940c8c2f57e
ZiadElgaraihe <ziadelgaraihe301@gmail.com>
2024-09-14 22:34:56 +0300

    create sign up profile info form & add validator param to custom animated dropdown

---
+++ lib/modules/auth/presentation/widgets/sign_up_profile_info_form.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:llegar/core/presentation/widgets/custom_animated_dropdown.dart';
+import 'package:llegar/core/presentation/widgets/custom_text_form_field.dart';
+import 'package:llegar/modules/auth/presentation/widgets/birth_date_text_form_field.dart';
+import 'package:llegar/utils/app_sizes.dart';
+import 'package:llegar/utils/functions/translate.dart';
+
+class SignUpProfileInfoForm extends StatelessWidget {
+  const SignUpProfileInfoForm({
+    super.key,
+    this.autovalidateMode,
+    this.formKey,
+  });
+
+  final AutovalidateMode? autovalidateMode;
+  final GlobalKey<FormState>? formKey;
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: formKey,
+      autovalidateMode: autovalidateMode,
+      child: Column(
+        children: [
+          AppSizes.height24,
+          CustomTextFormField(
+            hintText: translate(context).fullName,
+          ),
+          AppSizes.height12,
+          CustomTextFormField(hintText: translate(context).userName),
+          AppSizes.height12,
+          BirthDateTextFormField(
+            onDatePicked: (date) {},
+          ),
+          AppSizes.height24,
+          CustomAnimatedDropdown<String>(
+            hintText: translate(context).gender,
+            items: <String>[
+              translate(context).male,
+              translate(context).female,
+            ],
+          ),
+          AppSizes.height24,
+          CustomAnimatedDropdown<String>(
+            hintText: translate(context).city,
+            items: <String>[
+              translate(context).jerusalem,
+              translate(context).baghdad,
+              translate(context).damascus,
+              translate(context).cairo,
+              translate(context).rabat,
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
