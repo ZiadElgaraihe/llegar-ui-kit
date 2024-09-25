875e5aa01c59e76c0343ecf477d99ed6c2d4b858
ZiadElgaraihe <ziadelgaraihe301@gmail.com>
2024-09-25 20:17:20 +0300

    modify comment

--- lib/core/presentation/widgets/custom_elevated_button.dart
+++ lib/core/presentation/widgets/custom_elevated_button.dart
@@ -4,12 +4,12 @@ import 'package:llegar/shared/constants/app_colors.dart';
 import 'package:llegar/shared/constants/app_text_styles.dart';
 import 'package:llegar/shared/utils/functions/future_delayed_navigator.dart';
 
-///The main button in the app You can perform tasks that require time 
-///or instantaneous tasks using onFuturePressed() and onPressed()
-///
-///onFuturePressed() has priority over onPressed()
-///so if you used both of them onFuturePressed() will be executed
 class CustomElevatedButton extends StatefulWidget {
+  ///The main button in the app You can perform tasks that require time
+  ///using onFuturePressed() or instantaneous tasks using onPressed()
+  ///
+  ///onFuturePressed() has priority over onPressed()
+  ///so if you used both of them onFuturePressed() will be executed
   const CustomElevatedButton({
     super.key,
     required this.title,
@@ -19,7 +19,8 @@ class CustomElevatedButton extends StatefulWidget {
 
   ///Used for future processes like log in, sign up, ...
   final Future<void> Function()? onFuturePressed;
-  ///Used for non future processes like navigation between views, 
+
+  ///Used for non future processes like navigation between views,
   ///appearing pop ups, ...
   final VoidCallback? onPressed;
   final String title;
