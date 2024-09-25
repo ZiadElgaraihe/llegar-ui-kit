7cfe986f3f1b1b926c53c55a1f95b97785000481
ZiadElgaraihe <ziadelgaraihe301@gmail.com>
2024-09-25 22:32:48 +0300

    create nav bar entity

---
+++ lib/core/domain/entities/nav_bar_entity.dart
@@ -0,0 +1,11 @@
+class NavBarEntity {
+  final String label;
+  final String icon;
+  final String activeIcon;
+
+  const NavBarEntity({
+    required this.label,
+    required this.icon,
+    required this.activeIcon,
+  });
+}
