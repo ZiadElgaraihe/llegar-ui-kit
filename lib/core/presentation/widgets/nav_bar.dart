a2ca804b13af00f714d51354db7a2755e92de2fe
ZiadElgaraihe <ziadelgaraihe301@gmail.com>
2024-09-26 20:02:11 +0300

    refactor nav bar widget

---
+++ lib/core/presentation/widgets/nav_bar.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:llegar/core/domain/entities/nav_bar_entity.dart';
+import 'package:llegar/shared/constants/app_colors.dart';
+import 'package:llegar/shared/utils/functions/value_based_on_theme.dart';
+
+class NavBar extends StatelessWidget {
+  const NavBar({
+    super.key,
+    required this.currentIndex,
+    required this.items,
+    required this.pageController,
+  });
+
+  final ValueNotifier<int> currentIndex;
+  final List<NavBarEntity> items;
+  final PageController pageController;
+
+  @override
+  Widget build(BuildContext context) {
+    return ValueListenableBuilder(
+      valueListenable: currentIndex,
+      builder: (context, currentIndex, child) => BottomNavigationBar(
+        currentIndex: currentIndex,
+        onTap: (value) {
+          this.currentIndex.value = value;
+          pageController.animateToPage(
+            value,
+            duration: const Duration(milliseconds: 300),
+            curve: Curves.easeIn,
+          );
+        },
+        items: items
+            .map(
+              (item) => BottomNavigationBarItem(
+                icon: Padding(
+                  padding: const EdgeInsets.only(bottom: 4.0),
+                  child: SvgPicture.asset(
+                    item.icon,
+                    height: 22,
+                    width: 22,
+                    colorFilter: valueBasedOnTheme<ColorFilter>(
+                      context,
+                      dark: const ColorFilter.mode(
+                        AppColors.orange,
+                        BlendMode.srcATop,
+                      ),
+                    ),
+                  ),
+                ),
+                activeIcon: Padding(
+                  padding: const EdgeInsets.only(bottom: 4.0),
+                  child: SvgPicture.asset(
+                    item.activeIcon,
+                    height: 22,
+                    width: 22,
+                    colorFilter: valueBasedOnTheme<ColorFilter>(
+                      context,
+                      dark: const ColorFilter.mode(
+                        AppColors.orange,
+                        BlendMode.srcATop,
+                      ),
+                    ),
+                  ),
+                ),
+                label: item.label,
+              ),
+            )
+            .toList(),
+      ),
+    );
+  }
+}
