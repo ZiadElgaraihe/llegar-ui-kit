a9dcddf9e0fecd107983abd98ff680e27ebfb946
ZiadElgaraihe <ziadelgaraihe301@gmail.com>
2024-09-26 20:02:28 +0300

    create nav bar view body

--- lib/core/domain/entities/nav_bar_entity.dart
+++ lib/core/domain/entities/nav_bar_entity.dart
@@ -1,11 +1,15 @@
+import 'package:flutter/material.dart';
+
 class NavBarEntity {
   final String label;
   final String icon;
   final String activeIcon;
+  final Widget page;
 
   const NavBarEntity({
     required this.label,
     required this.icon,
     required this.activeIcon,
+    required this.page,
   });
 }
