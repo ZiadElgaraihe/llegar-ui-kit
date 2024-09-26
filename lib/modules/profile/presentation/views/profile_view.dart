441be77db7e82b032bb1afcd408aa30626c54b01
ZiadElgaraihe <ziadelgaraihe301@gmail.com>
2024-09-26 22:34:08 +0300

    create profile app bar

--- lib/modules/profile/presentation/views/profile_view.dart
+++ lib/modules/profile/presentation/views/profile_view.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:llegar/modules/profile/presentation/widgets/profile_app_bar.dart';
 
 class ProfileView extends StatelessWidget {
   const ProfileView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(
+      appBar: ProfileAppBar(),
+    );
   }
 }
