30718ece86f00bca0a4664194ec202d71d0c1b7d
ZiadElgaraihe <ziadelgaraihe301@gmail.com>
2024-09-27 22:18:01 +0300

    add listen property to value based on theme function

--- lib/shared/utils/functions/value_based_on_theme.dart
+++ lib/shared/utils/functions/value_based_on_theme.dart
@@ -2,12 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:llegar/core/presentation/manager/cubits/theme_cubit/theme_cubit.dart';
 
-///This function is responsible for checking for current theme 
+///This function is responsible for checking for current theme
 ///and return value depending on it
 T? valueBasedOnTheme<T>(
   BuildContext context, {
   T? light,
   T? dark,
+  bool listen = false,
 }) {
-  return BlocProvider.of<ThemeCubit>(context).isLight() ? light : dark;
+  return BlocProvider.of<ThemeCubit>(context, listen: listen).isLight()
+      ? light
+      : dark;
 }
