1325ef97ac8368e925798dc9574258334454cbcb
ZiadElgaraihe <ziadelgaraihe301@gmail.com>
2024-09-28 19:32:50 +0300

    create log out bottom sheet widget

---
+++ lib/modules/profile/presentation/widgets/log_out_bottom_sheet.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:llegar/core/presentation/widgets/custom_elevated_button.dart';
+import 'package:llegar/core/presentation/widgets/custom_secondary_elevated_button.dart';
+import 'package:llegar/shared/constants/app_colors.dart';
+import 'package:llegar/shared/constants/app_routes.dart';
+import 'package:llegar/shared/constants/app_sizes.dart';
+import 'package:llegar/shared/constants/app_text_styles.dart';
+import 'package:llegar/shared/utils/functions/translate.dart';
+
+class LogOutBottomSheet extends StatelessWidget {
+  const LogOutBottomSheet({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: AppSizes.bodyHorizontalPadding(context),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text(
+            translate(context).logOut,
+            style: AppTextStyles.bold24(context).copyWith(
+              color: AppColors.red,
+            ),
+          ),
+          const Divider(
+            thickness: 1,
+            color: AppColors.antiFlashWhite,
+            height: 32,
+          ),
+          FittedBox(
+            fit: BoxFit.scaleDown,
+            child: Text(
+              translate(context).areYouSureYouWantToLogOut,
+              style: AppTextStyles.bold20(context),
+            ),
+          ),
+          const Spacer(),
+          Row(
+            children: [
+              Flexible(
+                child: CustomSecondaryElevatedButton(
+                  title: translate(context).cancel,
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                ),
+              ),
+              AppSizes.width36,
+              Flexible(
+                child: CustomElevatedButton(
+                  title: translate(context).yes,
+                  onPressed: () {
+                    Navigator.pushNamedAndRemoveUntil(
+                      context,
+                      AppRoutes.socialLogInView,
+                      (route) => false,
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
+          AppSizes.height24,
+        ],
+      ),
+    );
+  }
+}
