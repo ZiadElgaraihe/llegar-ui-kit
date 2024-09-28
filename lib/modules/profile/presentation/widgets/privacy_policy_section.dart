fb4975834cd6a1f741b41261852e31c9c232e6df
ZiadElgaraihe <ziadelgaraihe301@gmail.com>
2024-09-28 14:53:23 +0300

    refactor code & create privacy policy section

---
+++ lib/modules/profile/presentation/widgets/privacy_policy_section.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:llegar/modules/profile/domain/entities/privacy_policy_entity.dart';
+import 'package:llegar/shared/constants/app_sizes.dart';
+import 'package:llegar/shared/constants/app_text_styles.dart';
+
+class PrivacyPolicySection extends StatelessWidget {
+  const PrivacyPolicySection({
+    super.key,
+    required this.pointNumber,
+    required this.privacyPolicyEntity,
+  });
+
+  final int pointNumber;
+  final PrivacyPolicyEntity privacyPolicyEntity;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          '$pointNumber. ${privacyPolicyEntity.title}',
+          style: AppTextStyles.bold20(context),
+        ),
+        AppSizes.height16,
+        Text(
+          privacyPolicyEntity.details,
+          style: AppTextStyles.medium14(context),
+        ),
+      ],
+    );
+  }
+}
