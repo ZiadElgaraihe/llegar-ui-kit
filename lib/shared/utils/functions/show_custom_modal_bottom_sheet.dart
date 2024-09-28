0a5d48431b0d28a8007ed11c0a58c01acf6ab23f
ZiadElgaraihe <ziadelgaraihe301@gmail.com>
2024-09-28 19:32:18 +0300

    create show custom modal bottom sheet function

---
+++ lib/shared/utils/functions/show_custom_modal_bottom_sheet.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+Future<T?> showCustomModalBottomSheet<T>(
+  BuildContext context, {
+  required Widget Function(BuildContext context) builder,
+  double height = 250,
+}) {
+  return showModalBottomSheet(
+    context: context,
+    constraints: BoxConstraints.expand(height: height),
+    enableDrag: true,
+    sheetAnimationStyle: AnimationStyle(
+      duration: const Duration(milliseconds: 500),
+      reverseDuration: const Duration(milliseconds: 500),
+    ),
+    builder: builder,
+  );
+}
