9c11c4aa25ebc2166c25f8dfa5de073568091b67
ZiadElgaraihe <ziadelgaraihe301@gmail.com>
2024-09-29 21:25:06 +0300

    create languaage entity

---
+++ lib/modules/profile/domain/entities/language_entity.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class LanguageEntity {
+  final String title;
+  final Locale value;
+
+  const LanguageEntity({
+    required this.title,
+    required this.value,
+  });
+}
