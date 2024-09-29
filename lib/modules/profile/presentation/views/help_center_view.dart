6b7af9d6fb8206a7da029d387189d3312c5e784f
ZiadElgaraihe <ziadelgaraihe301@gmail.com>
2024-09-29 23:06:57 +0300

    finish help center view body

--- lib/modules/profile/presentation/views/help_center_view.dart
+++ lib/modules/profile/presentation/views/help_center_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:llegar/core/presentation/widgets/custom_app_bar.dart';
+import 'package:llegar/modules/profile/presentation/widgets/help_center_view_body.dart';
 import 'package:llegar/shared/utils/functions/translate.dart';
 
 class HelpCenterView extends StatelessWidget {
@@ -11,6 +12,7 @@ class HelpCenterView extends StatelessWidget {
       appBar: CustomAppBar(
         title: translate(context).helpCenter,
       ),
+      body: const HelpCenterViewBody(),
     );
   }
 }
---
+++ lib/modules/profile/presentation/widgets/help_center_view_body.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:llegar/modules/profile/presentation/widgets/faq_section.dart';
+import 'package:llegar/modules/profile/presentation/widgets/help_center_tab_bar.dart';
+import 'package:llegar/shared/constants/app_sizes.dart';
+
+class HelpCenterViewBody extends StatelessWidget {
+  const HelpCenterViewBody({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: AppSizes.bodyHorizontalPadding(context),
+      child: const DefaultTabController(
+        length: 2,
+        child: Column(
+          children: [
+            AppSizes.height24,
+            HelpCenterTabBar(),
+            Expanded(
+              child: TabBarView(
+                children: [
+                  FaqSection(),
+                  Center(child: Text('ANS')),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
