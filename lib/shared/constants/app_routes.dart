e477d74caa5c8a10b948049c1511fed14f4542db
ZiadElgaraihe <ziadelgaraihe301@gmail.com>
2024-09-30 20:01:29 +0300

    create edit profile view

--- lib/shared/constants/app_routes.dart
+++ lib/shared/constants/app_routes.dart
@@ -11,6 +11,7 @@ import 'package:llegar/modules/auth/presentation/views/sign_up_profile_info_view
 import 'package:llegar/modules/auth/presentation/views/sign_up_view.dart';
 import 'package:llegar/modules/auth/presentation/views/social_log_in_view.dart';
 import 'package:llegar/modules/profile/presentation/views/contact_us_view.dart';
+import 'package:llegar/modules/profile/presentation/views/edit_profile_view.dart';
 import 'package:llegar/modules/profile/presentation/views/help_center_view.dart';
 import 'package:llegar/modules/profile/presentation/views/language_view.dart';
 import 'package:llegar/modules/profile/presentation/views/privacy_policy_view.dart';
@@ -40,6 +41,7 @@ abstract class AppRoutes {
   static const String contactUsView = '/ContactUsView';
   static const String languageView = '/LanguageView';
   static const String helpCenterView = '/HelpCenterView';
+  static const String editProfileView = '/EditProfileView';
 
   static Route<dynamic>? generate(RouteSettings routeSettings) {
     switch (routeSettings.name) {
@@ -92,6 +94,10 @@ abstract class AppRoutes {
       case navBarView:
         return BottomSlideTransition(
           page: const NavBarView(),
+        );
+      case editProfileView:
+        return SideSlideTransition(
+          page: const EditProfileView(),
         );
       case securityView:
         return SideSlideTransition(
