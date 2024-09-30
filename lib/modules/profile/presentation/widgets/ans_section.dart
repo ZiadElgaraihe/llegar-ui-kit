9bea3c0c840dc543e282f11655fa40e64adc0a8c
ZiadElgaraihe <ziadelgaraihe301@gmail.com>
2024-09-30 19:38:22 +0300

    create ans section

---
+++ lib/modules/profile/presentation/widgets/ans_section.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:llegar/modules/profile/presentation/widgets/ans_container.dart';
+import 'package:llegar/shared/constants/app_sizes.dart';
+
+class AnsSection extends StatelessWidget {
+  const AnsSection({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.separated(
+      padding: const EdgeInsets.symmetric(vertical: 24),
+      itemCount: 3,
+      itemBuilder: (context, index) => AnsContainer(answerNumber: index + 1),
+      separatorBuilder: (context, index) => AppSizes.height16,
+    );
+  }
+}
