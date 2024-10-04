2a03c86007816f9000dd4e7659005c7b6193b42d
ZiadElgaraihe <ziadelgaraihe301@gmail.com>
2024-10-04 22:57:41 +0300

    create subscribe to premium view body

--- lib/modules/profile/presentation/views/subscribe_to_premium_view.dart
+++ lib/modules/profile/presentation/views/subscribe_to_premium_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:llegar/core/presentation/widgets/custom_app_bar.dart';
+import 'package:llegar/modules/profile/presentation/widgets/subscribe_to_premium_view_body.dart';
 import 'package:llegar/shared/utils/functions/translate.dart';
 
 class SubscribeToPremiumView extends StatelessWidget {
@@ -12,6 +13,7 @@ class SubscribeToPremiumView extends StatelessWidget {
         title: translate(context).back,
         titleSpacing: 0,
       ),
+      body: const SubscribeToPremiumViewBody(),
     );
   }
 }
