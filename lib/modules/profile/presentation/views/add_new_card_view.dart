e75866213136a8d630cbeaa26707016df5f7c554
ZiadElgaraihe <ziadelgaraihe301@gmail.com>
2024-10-06 22:50:33 +0300

    finish add new card view design

--- lib/modules/profile/presentation/views/add_new_card_view.dart
+++ lib/modules/profile/presentation/views/add_new_card_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:llegar/core/presentation/widgets/custom_app_bar.dart';
+import 'package:llegar/modules/profile/presentation/widgets/add_new_card_view_body.dart';
 import 'package:llegar/shared/utils/functions/translate.dart';
 
 class AddNewCardView extends StatelessWidget {
@@ -7,9 +8,13 @@ class AddNewCardView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: CustomAppBar(
-        title: translate(context).addNewCard,
+    return GestureDetector(
+      onTap: () => FocusManager.instance.primaryFocus!.unfocus(),
+      child: Scaffold(
+        appBar: CustomAppBar(
+          title: translate(context).addNewCard,
+        ),
+        body: const AddNewCardViewBody(),
       ),
     );
   }
