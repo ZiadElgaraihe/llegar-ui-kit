b0e05bfa5ed02aca73d148727236efa3fda3d39e
ZiadElgaraihe <ziadelgaraihe301@gmail.com>
2024-10-09 22:54:59 +0300

    add medium 12 to app text styles

--- lib/shared/constants/app_text_styles.dart
+++ lib/shared/constants/app_text_styles.dart
@@ -17,6 +17,13 @@ abstract class AppTextStyles {
         fontWeight: FontWeight.w400,
       );
 
+  static TextStyle medium12(BuildContext context, {bool responsive = false}) =>
+      TextStyle(
+        fontSize:
+            responsive ? _getResponsiveFontSize(context, fontSize: 12) : 12,
+        fontWeight: FontWeight.w500,
+      );
+
   static TextStyle medium14(BuildContext context, {bool responsive = false}) =>
       TextStyle(
         fontSize:
