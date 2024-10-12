2eac38e47c9ab2030c36f222fe049dfd402cdf54
ZiadElgaraihe <ziadelgaraihe301@gmail.com>
2024-10-12 16:14:39 +0300

    add regular 12 & bold 12 to app text styles

--- lib/shared/constants/app_text_styles.dart
+++ lib/shared/constants/app_text_styles.dart
@@ -17,11 +17,25 @@ abstract class AppTextStyles {
         fontWeight: FontWeight.w400,
       );
 
+  static TextStyle regular12(BuildContext context, {bool responsive = false}) =>
+      TextStyle(
+        fontSize:
+            responsive ? _getResponsiveFontSize(context, fontSize: 12) : 12,
+        fontWeight: FontWeight.w400,
+      );
+
   static TextStyle medium12(BuildContext context, {bool responsive = false}) =>
       TextStyle(
         fontSize:
             responsive ? _getResponsiveFontSize(context, fontSize: 12) : 12,
         fontWeight: FontWeight.w500,
+      );
+
+  static TextStyle bold12(BuildContext context, {bool responsive = false}) =>
+      TextStyle(
+        fontSize:
+            responsive ? _getResponsiveFontSize(context, fontSize: 12) : 12,
+        fontWeight: FontWeight.w700,
       );
 
   static TextStyle medium14(BuildContext context, {bool responsive = false}) =>
