181cb4c268b1570ed90e8a91bc9ecde05d5262a9
ZiadElgaraihe <ziadelgaraihe301@gmail.com>
2024-10-12 19:29:39 +0300

    refactor my items code

--- lib/modules/profile/presentation/widgets/my_items_view_body.dart
+++ lib/modules/profile/presentation/widgets/my_items_view_body.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:llegar/core/presentation/widgets/item_widget.dart';
-import 'package:llegar/shared/constants/app_sizes.dart';
+import 'package:llegar/core/presentation/widgets/responsive_items_list.dart';
 
 class MyItemsViewBody extends StatelessWidget {
   const MyItemsViewBody({
@@ -9,29 +8,9 @@ class MyItemsViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    if (MediaQuery.sizeOf(context).width < AppSizes.mediumtBreakpoint) {
-      return ListView.separated(
-        padding: AppSizes.bodySymmetricPadding(context),
-        itemCount: 9,
-        itemBuilder: (context, index) => const ItemWidget(
-          appearLikeButton: false,
-        ),
-        separatorBuilder: (context, index) => AppSizes.height24,
-      );
-    } else {
-      return GridView.builder(
-        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-          maxCrossAxisExtent: 370,
-          mainAxisSpacing: 8,
-          crossAxisSpacing: 16,
-          childAspectRatio: 382 / 500,
-        ),
-        padding: AppSizes.bodySymmetricPadding(context),
-        itemCount: 9,
-        itemBuilder: (context, index) => const ItemWidget(
-          appearLikeButton: false,
-        ),
-      );
-    }
+    return const ResponsiveItemsList(
+      appearLikeButton: false,
+      itemCount: 9,
+    );
   }
 }
