7d21f9701ab0797405601adaed3270b57b7c7fd5
ZiadElgaraihe <ziadelgaraihe301@gmail.com>
2024-10-13 21:29:26 +0300

    create responsive wallet top up list tiles

---
+++ lib/modules/wallet/presentation/widgets/responsive_wallet_top_up_list_tiles.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:llegar/modules/wallet/presentation/widgets/wallet_top_up_list_tile.dart';
+import 'package:llegar/shared/constants/app_sizes.dart';
+
+class ResponsiveWalletTopUpListTiles extends StatelessWidget {
+  const ResponsiveWalletTopUpListTiles({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    if (MediaQuery.sizeOf(context).width < 1100) {
+      return Column(
+        children: List.generate(
+          3,
+          (index) => const WalletTopUpListTile(),
+        ),
+      );
+    } else {
+      return Row(
+        children: [
+          for (int i = 0; i < 3; i++) ...[
+            const Expanded(child: WalletTopUpListTile()),
+            if (i < 2) AppSizes.width8,
+          ]
+        ],
+      );
+    }
+  }
+}
