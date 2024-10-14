5683fe826a6c66063e22cba2c5b0c08fa094aefa
ZiadElgaraihe <ziadelgaraihe301@gmail.com>
2024-10-14 19:31:01 +0300

    finish fund my wallet design

--- lib/modules/wallet/presentation/views/fund_my_wallet_view.dart
+++ lib/modules/wallet/presentation/views/fund_my_wallet_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:llegar/core/presentation/widgets/custom_app_bar.dart';
+import 'package:llegar/modules/wallet/presentation/widgets/fund_my_wallet_view_body.dart';
 import 'package:llegar/shared/utils/functions/translate.dart';
 
 class FundMyWalletView extends StatelessWidget {
@@ -7,9 +8,13 @@ class FundMyWalletView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: CustomAppBar(
-        title: translate(context).fundMyWallet,
+    return GestureDetector(
+      onTap: () => FocusManager.instance.primaryFocus!.unfocus(),
+      child: Scaffold(
+        appBar: CustomAppBar(
+          title: translate(context).fundMyWallet,
+        ),
+        body: const FundMyWalletViewBody(),
       ),
     );
   }
---
+++ lib/modules/wallet/presentation/widgets/fund_my_wallet_view_body.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+import 'package:llegar/core/presentation/widgets/custom_sliver_fill_remaining_footer.dart';
+import 'package:llegar/modules/wallet/presentation/widgets/fund_amount_buttons_wrap.dart';
+import 'package:llegar/modules/wallet/presentation/widgets/fund_wallet_text_field.dart';
+import 'package:llegar/shared/constants/app_sizes.dart';
+import 'package:llegar/shared/constants/app_text_styles.dart';
+import 'package:llegar/shared/utils/functions/translate.dart';
+
+class FundMyWalletViewBody extends StatefulWidget {
+  const FundMyWalletViewBody({
+    super.key,
+  });
+
+  @override
+  State<FundMyWalletViewBody> createState() => _FundMyWalletViewBodyState();
+}
+
+class _FundMyWalletViewBodyState extends State<FundMyWalletViewBody> {
+  late TextEditingController _controller;
+  late ValueNotifier<String?> _selectedAmount;
+
+  @override
+  void initState() {
+    super.initState();
+    _selectedAmount = ValueNotifier(null);
+    _controller = TextEditingController(text: '\$');
+    _controller.addListener(_controllerListener);
+  }
+
+  @override
+  void dispose() {
+    _controller.removeListener(_controllerListener);
+    _controller.dispose();
+    _selectedAmount.dispose();
+    super.dispose();
+  }
+
+  void _controllerListener() {
+    _enforceDollarPrefix();
+    _deselectAmount();
+  }
+
+  ///Ensure that the user adheres to the requirement of commencing
+  ///the text input with the symbol "$"
+  void _enforceDollarPrefix() {
+    if (!_controller.text.startsWith('\$')) {
+      _controller.text = '\$${_controller.text}';
+      _controller.selection = const TextSelection.collapsed(offset: 1);
+    }
+  }
+
+  /// Deselect the selected amount if the user manually changes the text
+  void _deselectAmount() {
+    if (_selectedAmount.value != null &&
+        _controller.text != '\$${_selectedAmount.value}') {
+      _selectedAmount.value = null;
+    }
+  }
+
+  void _onFundAmountButtonTapped(selected) {
+    _controller.text = selected;
+    _selectedAmount.value = selected;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: AppSizes.bodyHorizontalPadding(context),
+      child: CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(
+            child: Column(
+              children: [
+                AppSizes.height16,
+                Text(
+                  translate(context).enterYourFundAmount,
+                  style: AppTextStyles.bold16(context),
+                ),
+                AppSizes.height16,
+                FundWalletTextField(
+                  controller: _controller,
+                ),
+                AppSizes.height16,
+                ValueListenableBuilder(
+                  valueListenable: _selectedAmount,
+                  builder: (context, selectedAmount, child) =>
+                      FundAmountButtonsWrap(
+                    onTap: _onFundAmountButtonTapped,
+                    selectedAmount: selectedAmount,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          CustomSliverFillRemainingFooter(
+            buttonTitle: translate(context).continueText,
+            onPressed: () {},
+          ),
+        ],
+      ),
+    );
+  }
+}
