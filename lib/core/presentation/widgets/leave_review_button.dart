2be86bb277a1bc7992c69891a156db7b863ea2d8
ZiadElgaraihe <ziadelgaraihe301@gmail.com>
2024-11-14 21:56:18 +0200

    refactor review button in profile summary view & use future delayed navigator

--- lib/core/presentation/widgets/leave_review_button.dart
+++ lib/core/presentation/widgets/leave_review_button.dart
@@ -2,12 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:llegar/core/presentation/widgets/review_bottom_sheet.dart';
 import 'package:llegar/shared/constants/app_icons.dart';
+import 'package:llegar/shared/utils/functions/future_delayed_navigator.dart';
 import 'package:llegar/shared/utils/functions/show_custom_modal_bottom_sheet.dart';
 
 class LeaveReviewButton extends StatelessWidget {
   const LeaveReviewButton({
     super.key,
+    this.colorFilter,
   });
+
+  final ColorFilter? colorFilter;
 
   @override
   Widget build(BuildContext context) {
@@ -15,15 +19,20 @@ class LeaveReviewButton extends StatelessWidget {
       onPressed: () {
         _onLeaveReviewPressed(context);
       },
-      icon: SvgPicture.asset(AppIcons.leaveReview),
+      icon: SvgPicture.asset(
+        AppIcons.leaveReview,
+        colorFilter: colorFilter,
+      ),
     );
   }
 
   void _onLeaveReviewPressed(BuildContext context) {
-    showCustomModalBottomSheet(
-      context,
-      height: 500,
-      builder: (context) => const ReviewBottomSheet(),
-    );
+    futureDelayedNavigator(() {
+      showCustomModalBottomSheet(
+        context,
+        height: 500,
+        builder: (context) => const ReviewBottomSheet(),
+      );
+    });
   }
 }
--- lib/core/presentation/widgets/profile_summary_view_body.dart
+++ lib/core/presentation/widgets/profile_summary_view_body.dart
@@ -1,15 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:llegar/core/domain/entities/chat_entity.dart';
 import 'package:llegar/core/domain/entities/profile_summary_entity.dart';
 import 'package:llegar/core/presentation/widgets/comment_widget.dart';
 import 'package:llegar/core/presentation/widgets/custom_elevated_button.dart';
+import 'package:llegar/core/presentation/widgets/leave_review_button.dart';
 import 'package:llegar/core/presentation/widgets/profile_summary_info_section.dart';
-import 'package:llegar/core/presentation/widgets/review_bottom_sheet.dart';
-import 'package:llegar/shared/constants/app_icons.dart';
 import 'package:llegar/shared/constants/app_routes.dart';
 import 'package:llegar/shared/constants/app_sizes.dart';
-import 'package:llegar/shared/utils/functions/show_custom_modal_bottom_sheet.dart';
 import 'package:llegar/shared/utils/functions/theme_colors.dart';
 import 'package:llegar/shared/utils/functions/translate.dart';
 
@@ -50,16 +47,10 @@ class ProfileSummaryViewBody extends StatelessWidget {
                       ),
                       // This button should only appear for profiles
                       // you have rented from before
-                      IconButton(
-                        onPressed: () {
-                          _onLeaveReviewPressed(context);
-                        },
-                        icon: SvgPicture.asset(
-                          AppIcons.leaveReview,
-                          colorFilter: ColorFilter.mode(
-                            mainThemeColor(context),
-                            BlendMode.srcATop,
-                          ),
+                      LeaveReviewButton(
+                        colorFilter: ColorFilter.mode(
+                          mainThemeColor(context),
+                          BlendMode.srcATop,
                         ),
                       ),
                     ],
@@ -105,14 +96,6 @@ class ProfileSummaryViewBody extends StatelessWidget {
         appBarTitle: 'Mohamed Galal',
         messages: [],
       ),
-    );
-  }
-
-  void _onLeaveReviewPressed(BuildContext context) {
-    showCustomModalBottomSheet(
-      context,
-      height: 500,
-      builder: (context) => const ReviewBottomSheet(),
     );
   }
 }
